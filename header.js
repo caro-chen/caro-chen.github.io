553e01729550d1a37e3ff76861951aa6dea3f904
Caroline Chen <carolinejchen@berkeley.edu>
2018-09-10 02:50:49 -0700

    updated navbar and pics

--- header.js
+++ header.js
@@ -15,3 +15,18 @@ function hideContent() {
         x.style.display = "none";
     }
 }
+
+jQuery(document).ready(function($){
+    $('.topnav').css("background-color", "transparent");
+    $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        $('.topnav').css("background-color", "white").css("transition","0.3s ease-in-out");
+        $('.topnav').css("border-bottom", "1px solid #c0c0c0").css("transition","0.3s ease-in-out");
+        $('.active').css("border-bottom", "2px solid #030e64").css("transition","0.3s ease-in-out");
+    } else {
+        $('.topnav').css("background-color", "transparent").css("transition","0.3s ease-in-out");
+        $('.topnav').css("border-bottom", "1px solid transparent").css("transition","0.3s ease-in-out");
+        $('.active').css("border-bottom", "2px solid transparent").css("transition","0.3s ease-in-out");
+    }
+    });
+});
